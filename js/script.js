291e00f8047fa0d21a9848785d5fe1809722532d
Benedettopana <benedetto.pana@gmail.com>
2024-02-26 14:50:26 +0100

    [add]MILESTONE#3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 const itemsWrapper = document.querySelector('.items-wrapper');
+// Creo le chevron
+const topArrow = document.querySelector('.top');
+const botArrow = document.querySelector('.bot');
+
+topArrow.classList.add('hide');
 
 const images = [
   './assets/img/01.webp',
@@ -7,9 +12,39 @@ const images = [
   './assets/img/04.webp',
   './assets/img/05.webp',
 ];
+// CONTATORE IMG
+let counterImg = 0;
 
 for(let i = 0; i < images.length; i++){
   const img = images[i];
   itemsWrapper.innerHTML += `<img class="img hide" src="${img}">`;
   console.log(img);
 }
+
+// Prendo tutti gli elementi con la classe img
+const itemsCollecion = document.getElementsByClassName('img');
+itemsCollecion[counterImg].classList.remove('hide');
+
+topArrow.addEventListener('click',function(){
+  botArrow.classList.remove('hide');
+
+  itemsCollecion[counterImg].classList.add('hide');
+  // itemsCollecion[counterImg--].classList.add('hide');
+  counterImg--;
+  itemsCollecion[counterImg].classList.remove('hide');
+  if(counterImg === 0){
+    topArrow.classList.add('hide');
+  }
+});
+
+botArrow.addEventListener('click',function(){
+  topArrow.classList.remove('hide');
+
+  itemsCollecion[counterImg++].classList.add('hide');
+
+  itemsCollecion[counterImg].classList.remove('hide');
+
+  if(counterImg === images.length-1){
+    botArrow.classList.add('hide');
+  }
+});
